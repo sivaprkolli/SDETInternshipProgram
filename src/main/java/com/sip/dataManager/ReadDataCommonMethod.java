323a6a987880fe57fd1946e969214af59e47229a
Siva <>
2024-04-23 09:20:14 +0530

    read data from excel

---
+++ src/main/java/com/sip/dataManager/ReadDataCommonMethod.java
@@ -0,0 +1,79 @@
+package com.sip.dataManager;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class ReadDataCommonMethod {
+
+    public String readTestData(String fileName, String sheetName, String rowCellvalue, String colCellValue) throws IOException {
+
+        File file = new File(System.getProperty("user.dir") + "/"+fileName);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet xssfSheet = xssfWorkbook.getSheet(sheetName);
+
+        int rowNumber = 0, colNumber = 0;
+
+        /**
+         * Iteration of all rows in the sheet
+         */
+
+        for(Row row: xssfSheet){
+            Cell cell = row.getCell(0); // first cell in the row
+            System.out.println(cell);
+
+            if(cell != null
+                    && cell.getCellType() == CellType.STRING
+                    && cell.getStringCellValue().equals(rowCellvalue)){
+                rowNumber = row.getRowNum();
+                System.out.println(rowNumber);
+            }
+        }
+
+        /**
+         * Iteration of cells on header row
+         */
+        Row headerRow = xssfSheet.getRow(0); // first cell in the header row
+        for(Cell cell: headerRow){
+
+            System.out.println(cell);
+
+            if(cell != null
+                    && cell.getCellType() == CellType.STRING
+                    && cell.getStringCellValue().equals(colCellValue)){
+                colNumber = cell.getColumnIndex();
+                System.out.println(colNumber);
+            }
+
+        }
+
+        Cell cellValue = xssfSheet.getRow(rowNumber).getCell(colNumber);
+
+        System.out.println(cellValue);
+        String actualCellValue = cellValue.toString();
+//        BigDecimal bigDecimal = new BigDecimal(actualCellValue);
+//        long valueX = bigDecimal.longValue();
+//        System.out.println(valueX);
+//        return String.valueOf(valueX);
+
+        return actualCellValue;
+    }
+
+    @Test
+    public void readDataFromExcel(){
+        try {
+            readTestData("CCInfo.xlsx", "ProdData", "User Name", "ProdCompany");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
